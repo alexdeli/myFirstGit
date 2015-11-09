679b45baa5ec914f652eb90494d101cf0d9058d3
Alex Deli <alex.a.deli@gmail.com>
2015-11-09 14:42:39 -0600

    added git

--- app.ts
+++ app.ts
@@ -1,3 +1,13 @@
-let tsVar = 10;
-console.log(tsVar);
-parseInt(tsVar);
+// This is a simple app that will add two numbers and return the value <--- this is a comment
+let x = 12;
+let y = 3;
+
+let result = addNumbers(x, y);
+
+// This function will add two numbers and return the result. <--- this is a comment
+function addNumbers(val1: number, val2:number) {
+    'use strict';
+
+    var oops = val1 + val2;
+    return oops;
+}
